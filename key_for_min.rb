aed42c8d9811455d041feb5a75d80b8d22e513fb
Erik Zhao <erikzhao98@gmail.com>
2020-07-30 16:51:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   lowest_value = nil
   # comparing lowest value to value this will determine which is the lowest value
-  name_hash.each do |key, value| 
-    if 
-    puts "#{key} is #{value}" 
+  name_hash.each do |key, value|
+    if
+    puts "#{key} is #{value}"
   end
   end
 end
